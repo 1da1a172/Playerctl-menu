fed89ce2615e47c7a9d059c9f672a78c1af5ea9a
KKV9 <ciaranob.cob@gmail.com>
2024-02-15 16:38:17 +0000

    Refactor
    Use switch statement instead of if-else block.
    Better comments and formatting.

--- Player.sh
+++ Player.sh
@@ -7,37 +7,49 @@ MENU="rofi"
 # Set menu prompt
 PROMPT="PlayerControl"
 
-# Set menu command arguments
-if [[ $MENU == "rofi" ]]; then
-	menu_args=(-dmenu -l 5 -p "$PROMPT")
-elif [[ $MENU == "fuzzel" || $MENU == "wofi" ]]; then
-	menu_args=(-d -l 5 -p "$PROMPT")
-elif [[ $MENU == "tofi" ]]; then
-	menu_args=(--prompt-text "$PROMPT")
-elif [[ $MENU == "dmenu" ]]; then
-	menu_args=(-p "$PROMPT")
-else
-	menu_args=()
-fi
-
 # Exit if no media is playing
 check=$(playerctl metadata)
 if [ -z $check ]; then
 	exit 0
 fi
 
+# Set menu arguments
+case $MENU in
+"rofi")
+	menu_args=(-dmenu -l 5 -p "$PROMPT")
+	;;
+"fuzzel" | "wofi")
+	menu_args=(-d -l 5 -p "$PROMPT")
+	;;
+"tofi")
+	menu_args=(--prompt-text "$PROMPT")
+	;;
+"dmenu")
+	menu_args=(-p "$PROMPT")
+	;;
+*)
+	menu_args=()
+	;;
+esac
+
 # Loop if option is selected
 while true; do
+
 	# Get currently playing
 	current=$(playerctl metadata --format "{{artist}} - {{title}}")
+
 	# Set menu options
 	opts=("⏸️ $current" "⏭️ next track" "⏮️ previous track" "➡️ shift source forward" "⬅️ shift source backword")
-	# Toggle pause/play
+
+	# Show play-pause status
 	if [[ $(playerctl status) != "Playing" ]]; then
 		opts[0]="▶️ $current"
 	fi
+
 	# Menu prompt
 	selection=$(printf '%s\n' "${opts[@]}" | $MENU ${menu_args[@]})
+
+	# Handle selection
 	case $selection in
 	"${opts[0]}")
 		playerctl play-pause
